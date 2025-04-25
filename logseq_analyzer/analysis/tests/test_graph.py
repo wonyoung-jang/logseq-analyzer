1e007bce8d6816fc5b8ef62b9f6e37c087335b41
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2025-04-24 21:19:16 -0400

    feat: add string representation and length tests for LogseqGraph, LogseqJournals, and LogseqNamespaces classes

--- logseq_analyzer/analysis/tests/test_graph.py
+++ logseq_analyzer/analysis/tests/test_graph.py
@@ -15,3 +15,14 @@ def logseq_graph():
 def test_singleton(logseq_graph):
     another_instance = LogseqGraph()
     assert logseq_graph is another_instance
+
+
+def test_representation(logseq_graph):
+    assert repr(logseq_graph) == "LogseqGraph()"
+    assert str(logseq_graph) == "LogseqGraph"
+
+
+def test_len(logseq_graph):
+    assert len(logseq_graph) == 0
+    logseq_graph.all_linked_references = {"a": 1, "b": 2}
+    assert len(logseq_graph) == 2
--- logseq_analyzer/analysis/tests/test_journals.py
+++ logseq_analyzer/analysis/tests/test_journals.py
@@ -15,3 +15,14 @@ def logseq_journals():
 def test_singleton(logseq_journals):
     another_instance = LogseqJournals()
     assert logseq_journals is another_instance
+
+
+def test_representation(logseq_journals):
+    assert repr(logseq_journals) == "LogseqJournals()"
+    assert str(logseq_journals) == "LogseqJournals"
+
+
+def test_len(logseq_journals):
+    assert len(logseq_journals) == 0
+    logseq_journals.complete_timeline = ["2023-01-01", "2023-01-02"]
+    assert len(logseq_journals) == 2
