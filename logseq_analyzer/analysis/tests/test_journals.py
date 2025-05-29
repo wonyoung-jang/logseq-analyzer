09051d8edbb467e18245127c099160b85a9dcf18
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2025-05-29 18:00:32 -0400

    refactor: update test_len to use timeline attribute instead of complete_timeline

--- logseq_analyzer/analysis/tests/test_journals.py
+++ logseq_analyzer/analysis/tests/test_journals.py
@@ -24,5 +24,5 @@ def test_representation(logseq_journals):
 
 def test_len(logseq_journals):
     assert len(logseq_journals) == 0
-    logseq_journals.complete_timeline = ["2023-01-01", "2023-01-02"]
+    logseq_journals.timeline = ["2023-01-01", "2023-01-02"]
     assert len(logseq_journals) == 2
